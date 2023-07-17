a51c31e079615f4e85aeaedf98a019274a71a316
kcurti <kiersten.curti@noaa.gov>
2023-07-16 23:45:22 -0400

    Update Analyze.Short.Term.Projection.Output.R
    update paths, etc

--- Analyze.Short.Term.Projection.Output.R
+++ Analyze.Short.Term.Projection.Output.R
@@ -8,20 +8,18 @@ ls()
 
 
 # Run details
-run.no <- '4'
-current.assess.dir <- c('C:/Users/kiersten.curti/Desktop/Work/Mackerel/2021.MT.Modeling')
+run.no <- '9'
+current.assess.dir <- c('C:/Users/kiersten.curti/Desktop/Work/Mackerel/2023.Management.Track')
 
 
 # Projection details
 
-# pstar.yr <- "2032.2nd.iter"
-rect.name <- 'Rect.2Stanza.90545mt' #   'Rect.2009' # 
-f.name <- 'Frebuild' # 'FMSY' # 
-CV <- 150
-proj.dir.name <- paste('rebuilding/Additional.Projections.March2022', 'F.rebuild', sep='/')
+#yc.name <- 'M04'
+rect.name <- 'Rect.2Stanza' #   'Rect.2009' # 
+f.name <- 'F12' # 'FMSY' # 
+
+proj.dir.name <- 'rebuilding'
 proj.fname <- paste('PROJECTIONS.THROUGH2032', toupper(rect.name), toupper(f.name), sep='.')
-# proj.dir.name <- paste('rebuilding/Additional.Projections.May2022/PStar.Constant.Catch.Combo', "CC.6316mt", paste(2020,pstar.yr,sep="-"), sep='/')
-# proj.fname <- paste('PROJECTIONS.THROUGH2032', toupper(rect.name), paste("CV",CV,sep=""), toupper(f.name), sep='.')
 
 
 ### Output details
@@ -29,10 +27,10 @@ proj.fname <- paste('PROJECTIONS.THROUGH2032', toupper(rect.name), toupper(f.nam
 quant.values <- c(0.025, 0.05, 0.40, 0.75, 0.95, 0.975)
 
 # Corresponding percentiles for summary table
-# table.percentiles <- c('5%','40%','75%','95%')
-#   names(table.percentiles) <- c('5th Percentile', '40th Percentile', '75th Percentile', '95th Percentile')
-table.percentiles <- c('5%','75%','95%')
-  names(table.percentiles) <- c('5th Percentile', '75th Percentile', '95th Percentile')
+table.percentiles <- c('5%','40%','75%','95%')
+  names(table.percentiles) <- c('5th Percentile', '40th Percentile', '75th Percentile', '95th Percentile')
+# table.percentiles <- c('5%','75%','95%')
+#   names(table.percentiles) <- c('5th Percentile', '75th Percentile', '95th Percentile')
   
   
 
@@ -47,16 +45,15 @@ run.dir <- file.path(current.assess.dir, paste('Run',run.no,sep=''))
 proj.master.folder <- paste('projections',proj.dir.name, sep='.')
 proj.master.dir <- file.path(run.dir, proj.master.folder)
 
-# proj.dir <- file.path(proj.master.dir, rect.name, f.name)
-proj.dir <- proj.master.dir
+# proj.dir <- file.path(proj.master.dir, yc.name, f.name)
+proj.dir <- file.path(proj.master.dir, f.name)
 
-setwd(proj.dir)
 
 
 
 ### Read in rdat file
 
-rdat <- dget(paste(proj.fname, 'RDAT', sep='.'))
+rdat <- dget(file.path(proj.dir, paste(proj.fname, 'RDAT', sep='.')))
 
 proj.fyr <- rdat$genparms$startyear
 proj.lyr <- rdat$genparms$endyear
@@ -67,31 +64,31 @@ proj.yrs <- as.character(proj.fyr:proj.lyr)
 ### Read in AGEPRO auxillary files
 
 # SSB (xx3)
-ssb <- read.table(paste(proj.fname,'xx3',sep='.'))
+ssb <- read.table(file.path(proj.dir, paste(proj.fname,'xx3',sep='.')))
   colnames(ssb) <- proj.yrs
 ssb.median <- apply(ssb,2,median)
 ssb.CI <- apply(ssb,2,function(x){quantile(x, quant.values) })
 
 # Combined catch (xx6)
-catch <- read.table(paste(proj.fname,'xx6',sep='.'))
+catch <- read.table(file.path(proj.dir, paste(proj.fname,'xx6',sep='.')))
   colnames(catch) <- proj.yrs
 catch.median <- apply(catch,2,median)
 catch.CI <- apply(catch,2,function(x){quantile(x, quant.values) })
 
 # Fmult (xx9)
-fmult <- read.table(paste(proj.fname,'xx9',sep='.'))
+fmult <- read.table(file.path(proj.dir, paste(proj.fname,'xx9',sep='.')))
   colnames(fmult) <- proj.yrs
 fmult.median <- apply(fmult,2,median)
 fmult.CI <- apply(fmult,2,function(x){quantile(x, quant.values) })
 
-# Stock Biomass (Jan-1) (xx4)
-biomass <- read.table(paste(proj.fname,'xx4',sep='.'))
-  colnames(biomass) <- proj.yrs
-biomass.median <- apply(biomass,2,median)
-biomass.CI <- apply(biomass,2,function(x){quantile(x, quant.values) })
+# # Stock Biomass (Jan-1) (xx4)
+# biomass <- read.table(file.path(proj.dir, paste(proj.fname,'xx4',sep='.')))
+#   colnames(biomass) <- proj.yrs
+# biomass.median <- apply(biomass,2,median)
+# biomass.CI <- apply(biomass,2,function(x){quantile(x, quant.values) })
 
 # Recruitment (xx2)
-rect <- read.table(paste(proj.fname,'xx2',sep='.'))
+rect <- read.table(file.path(proj.dir, paste(proj.fname,'xx2',sep='.')))
   colnames(rect) <- proj.yrs
 rect.median <- apply(rect,2,median)
 rect.CI <- apply(rect,2,function(x){quantile(x, quant.values) })
@@ -126,15 +123,16 @@ create.summary.table <- function(var.name, var.label, unit.scalar, round.digits)
 ssb.table   <- create.summary.table('ssb', 'SSB (mt)', 1, 0)
 catch.table <- create.summary.table('catch', 'Catch (mt)', 1, 0)
 f.table     <- create.summary.table('fmult','F', 1, 2)
-biomass.table <- create.summary.table('biomass','January 1 biomass (mt)', 1, 0)
+# biomass.table <- create.summary.table('biomass','January 1 biomass (mt)', 1, 0)
 rect.table  <- create.summary.table('rect','Recruitment (000s)', 1000, 0)
 
-combined.table <- rbind.data.frame(ssb.table, f.table, catch.table, rect.table, biomass.table)
+# combined.table <- rbind.data.frame(ssb.table, f.table, catch.table, rect.table, biomass.table)
+combined.table <- rbind.data.frame(ssb.table, f.table, catch.table, rect.table)
 
 
 ### Save image and export CSV file
 
-rm(ssb, catch, fmult, biomass, rect)
+rm(ssb, catch, fmult, rect)
 # save.image(file.path(proj.dir, 'Projection.summary.RDATA'))
 # write.csv(combined.table,file.path(proj.dir, 'Short.term.projection.summary.csv'), na='')
 
